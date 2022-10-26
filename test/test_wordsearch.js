7f5e179c97bc955551dcebb275f118bccc93bfd0
Cheever Esler <ames.esler@gmail.com>
2022-10-26 20:29:03 -0300

    completed exercise

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,25 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'C', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'R', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'N', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'C', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'H', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'Y', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CRUNCHY')
+
+    assert.isTrue(result);
+  });
+  it("should return false if the array is empty", function() {
+    const result = wordSearch([], 'CRUNCHY')
+
+    assert.isFalse(result);
+  });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
+//don't need to check if word is backwards or 
+
+// PAIR PROGRAMMED WITH DAVID FIGUEROA
+
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
+    const verticalArray = [];
+    if (letters.length === 0) {
+        return false;
+    }
     for (l of horizontalJoin) {
+        
         if (l.includes(word)) return true
     }
+    for (let c = 0; c < letters[0].length; c++) {
+      let vertArray = [];
+      for (let r = 0; r < letters.length; r++) {
+        vertArray.push(letters[r][c])
+      }
+      verticalArray.push(vertArray);
+
+    }
+    const verticalJoin = verticalArray.map(ls => ls.join(''))
+    for (l of verticalJoin) {
+        
+        if (l.includes(word)) return true
+    }
+    
+    return false;
 }
 
 module.exports = wordSearch
